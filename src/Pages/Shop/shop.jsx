3e99f6caa77cfae893ae0b35c49cdae0104921a6
DotmanL <60046089+DotmanL@users.noreply.github.com>
2020-03-07 22:34:12 +0100

    added collection styles

--- src/Pages/Shop/shop.jsx
+++ src/Pages/Shop/shop.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import SHOP_DATA from './shop.data'
-import CollectionPreview from '../../components/collection-preview/collection-preview'
+import React from 'react';
 
-class ShopPage extends React.Component{
-    constructor(){
-      super();
-      this.state ={
-   collections: SHOP_DATA
-      };
-    }
+import SHOP_DATA from './shop.data.js';
 
-render() {
-  const {collections} = this.state
-  return (
-  <div className ='shop-page'>
-    {
-    collections.map(({ id, ...otherCollectionProps } ) => (
-      <CollectionPreview key ={id} {...otherCollectionProps} />
-    ))
+import CollectionPreview from '../../components/collection-preview/collection-preview';
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
-  </div>
-  );
-}
 
-}
-
-    export default ShopPage;
+export default ShopPage;
