377ed842517f20db1624410e15077ae0e25ce792
Lawal Oladotun <oladotunlawal7@gmail.com>
2020-03-17 16:52:37 +0100

    Update shop.jsx

--- src/Pages/Shop/shop.jsx
+++ src/Pages/Shop/shop.jsx
@@ -5,7 +5,7 @@ import CollectionPage from '../Collection/collection';
 
 const ShopPage = ({ match }) => (
       <div className='shop-page'>
-        < Route exact path ={`${match.path}`} component= {CollectionsOverview} />
+        <Route exact path ={`${match.path}`} component= {CollectionsOverview} />
         <Route path = {`${match.path}/:collectionId`} component={CollectionPage}   />
       </div>
     );
