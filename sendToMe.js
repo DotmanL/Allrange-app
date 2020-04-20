925bc6bdea7e541310a76379d9294fac805c0ba9
DotmanL <60046089+DotmanL@users.noreply.github.com>
2020-04-20 21:01:40 +0100

    configured smtp transporter with oauth2 client

--- sendToMe.js
+++ sendToMe.js
@@ -1,19 +1,38 @@
 const express = require('express')
 const sendToMeRouter = express.Router()
 const nodemailer = require('nodemailer')
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
 
-const transport = {
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.THE_EMAIL,
-      pass: process.env.THE_PASSWORD
-    }
-  };
 
-  const transporter = nodemailer.createTransport(transport);
+const oauth2Client = new OAuth2(
+  process.env.ClientID,
+  process.env.ClientSecret,
+ "https://developers.google.com/oauthplayground", 
+);
+
+oauth2Client.setCredentials({
+  refresh_token: `1//042nj6Cs1mkjlCgYIARAAGAQSNwF-L9IrXUVSHYpVyfXcprKb5Fg7GURFHc2AvSXOrmTr5Nv4ARLcOlwIUBG5vOhypCiKMHenZGk`
+});
+
+//const accessToken = oauth2Client.getAccessToken()
+
+const smtpConfig = {
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+       type: "OAuth2",
+       user: "oladotunlawal7@gmail.com", 
+       ClientId: process.env.ClientID,
+       ClientSecret: process.env.ClientSecret,
+       RefreshToken: process.env.RefreshToken,
+       accessToken: process.env.AccessToken
+  }
+};
+
+  const transporter = nodemailer.createTransport(smtpConfig);
   transporter.verify((error, success) => {
     if(error) {
       console.error(error)
